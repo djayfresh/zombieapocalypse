5c1502f4b88be59021d5776745f1896225f00e9b
djayfresh <djfresh1@comcast.net>
2018-07-16 18:27:11 -0500

    Get game loop running

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+var documentWidth = window.innerWidth;
+
 let CONFIG = {
-    width: 800,
+    width: documentWidth * 0.75,
     height: 800
 }
-var documentWidth = window.innerWidth;
 
 
 let type = "WebGL";
@@ -11,47 +12,56 @@ if(!PIXI.utils.isWebGLSupported()){
 }
 
 var Game = {
+    app: null,
+
+    setup: function() {
+        this.app = new PIXI.Application({width: CONFIG.width, height: CONFIG.height});
+        
+        this.app.view.style.left = (documentWidth/2) - (CONFIG.width/2);
+        this.app.view.className = 'my-game';
+    },
+
+    start: function() {
+        this.state = this.play;
+
+        //Start the game loop by adding the `gameLoop` function to
+        //Pixi's `ticker` and providing it with a `delta` argument.
+        this.app.ticker.add(dt => this.state(dt));
+    },
 
     update: function(dt) {
-        
+
     },
 
-    run: function(dt) {
-        Game.update(dt);
+    pause: function(dt) {
+        this.state = function() {};
     },
 
-    pause: function() {
-        Game.state = function() {};
+    play: function(dt) {
+        this.update(dt);
     },
 
-    play: function() {
-        Game.state = this.run;
+    resize: function() {
+        var gameWidth = (window.innerWidth * 0.75);
+        this.app.view.style.left = (window.innerWidth/2) - (gameWidth/2);
+        this.app.resize(gameWidth, CONFIG.height);
     },
 
     state: null,
     HighSchore: 0,
 }
 
-Game.state = Game.play;
 
-let app = new PIXI.Application({width: CONFIG.width, height: CONFIG.height});
-
-app.view.style.left = (documentWidth/2) - (CONFIG.width/2);
-app.view.className = 'my-game';
-
-
-
-
-
-
-
-
-
-
-
-
+window.addEventListener("resize", function() {
+    Game.resize();
+}, false);
 
 document.addEventListener("DOMContentLoaded", function(event) {
+
+    Game.setup();
+
     //Add the canvas that Pixi automatically created for you to the HTML document
-    document.body.appendChild(app.view);
+    document.body.appendChild(Game.app.view);
+
+    Game.start();
 });
